3d996ace15a3f2f5ec7c9df941d147398ba4cc1b
Chen Zhen <zhen.chen@smartswarm.org>
2018-06-10 00:49:18 +0800

    add api: "short"

--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -9,6 +10,7 @@ import (
 func Init() {
 	fmt.Println("~~~")
 	http.HandleFunc("/", index)
+	http.HandleFunc("/short", api_short)
 
 	err := http.ListenAndServe("0.0.0.0:9999", nil)
 	if err != nil {
@@ -18,4 +20,20 @@ func Init() {
 
 func index(resp http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(resp, "hello go!")
+}
+
+func api_short(resp http.ResponseWriter, req *http.Request) {
+	if req.Method == "PUT" {
+		log.Println("[server] handle api \"short\",", req.Method)
+		result, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			resp.WriteHeader(500)
+		} else {
+			origin_url := string(result)
+			// TODO: encode origin url here.
+			fmt.Fprintln(resp, origin_url)
+		}
+	} else {
+		resp.WriteHeader(404)
+	}
 }
