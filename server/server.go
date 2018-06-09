31222249b70de08200d2dc9f2775c64ded0f1ee3
Chen Zhen <zhen.chen@smartswarm.org>
2018-06-10 01:03:35 +0800

    test redis

--- server/server.go
+++ server/server.go
@@ -5,13 +5,26 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
+
+	"github.com/go-redis/redis"
 )
 
 func Init() {
-	fmt.Println("~~~")
+	fmt.Print("[server] init redis...")
+	redis.NewClient(&redis.Options{
+		Addr:     "127.0.0.1:6379",
+		Password: "",
+		DB:       0,
+	})
+	fmt.Print("OK\n")
+
+	fmt.Print("[server] init http...")
 	http.HandleFunc("/", index)
 	http.HandleFunc("/short", api_short)
 
+	fmt.Print("OK\n")
+	fmt.Println("[server] start listening...")
 	err := http.ListenAndServe("0.0.0.0:9999", nil)
 	if err != nil {
 		log.Fatal("http server start failed!", err)
@@ -29,9 +42,13 @@ func api_short(resp http.ResponseWriter, req *http.Request) {
 		if err != nil {
 			resp.WriteHeader(500)
 		} else {
-			origin_url := string(result)
-			// TODO: encode origin url here.
-			fmt.Fprintln(resp, origin_url)
+			origin_url := strings.TrimSpace(string(result))
+			if len(origin_url) <= 0 {
+				resp.WriteHeader(500)
+			} else {
+				// TODO: encode origin url here.
+				fmt.Fprintln(resp, origin_url)
+			}
 		}
 	} else {
 		resp.WriteHeader(404)
