61f7074dfa1fc9e32dd1c1b48e16fb86fc0b8bfd
Chen Zhen <zhen.chen@smartswarm.org>
2018-06-10 01:12:35 +0800

    test redis,  add record

--- server/server.go
+++ server/server.go
@@ -10,9 +10,11 @@ import (
 	"github.com/go-redis/redis"
 )
 
+var redis_client *redis.Client
+
 func Init() {
 	fmt.Print("[server] init redis...")
-	redis.NewClient(&redis.Options{
+	redis_client = redis.NewClient(&redis.Options{
 		Addr:     "127.0.0.1:6379",
 		Password: "",
 		DB:       0,
@@ -44,10 +46,24 @@ func api_short(resp http.ResponseWriter, req *http.Request) {
 		} else {
 			origin_url := strings.TrimSpace(string(result))
 			if len(origin_url) <= 0 {
+				log.Fatal("[server] bad url data", origin_url)
 				resp.WriteHeader(500)
 			} else {
-				// TODO: encode origin url here.
-				fmt.Fprintln(resp, origin_url)
+				ret := redis_client.Incr("short_url_id")
+				if ret.Err() != nil {
+					log.Fatal("[server] failed to prepare record", ret.Err())
+					resp.WriteHeader(500)
+				} else {
+					url_id := ret.Val()
+					ret := redis_client.HSet("url", string(url_id), origin_url)
+					if ret.Err() != nil {
+						log.Fatal("[server] failed to add record", ret.Err())
+						resp.WriteHeader(500)
+					} else {
+						// TODO: 把id编码构造url下发
+						fmt.Fprintln(resp, "id:", url_id)
+					}
+				}
 			}
 		}
 	} else {
